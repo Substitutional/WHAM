43f7f004e2e3542a30468cbf27da49c9342d4e47
Substitutional <32260164+Substitutional@users.noreply.github.com>
2018-09-26 14:01:32 +0200

    Update quiz2.sql
    rättat queryn och formaterat svaret

--- quiz2.sql
+++ quiz2.sql
@@ -1,16 +1,16 @@
 -- Query
 
 
-SELECT sumProduct.productLine, 
-SUM(sumProduct.quantityInStock) AS productLineQuantitySum
-FROM (SELECT productName, quantityInStock, productLine FROM classicmodels.products) AS sumProduct
-GROUP BY sumProduct.productline;
+SELECT products.productLine, 
+FORMAT(SUM(products.quantityInStock*products.buyPrice),2) AS productLineQuantitySum
+FROM classicmodels.products
+GROUP BY products.productline;
 -- result--
-#productLine, productLineQuantitySum
-'Classic Cars','219183'
-'Motorcycles','69401'
-'Planes','62287'
-'Ships','26833'
-'Trains','16696'
-'Trucks and Buses','35851'
-'Vintage Cars','124880'
+# productLine, productLineQuantitySum
+'Classic Cars', '14,059,337.71'
+'Motorcycles', '3,565,714.18'
+'Planes', '3,099,282.76'
+'Ships', '1,239,140.43'
+'Trains', '727,251.77'
+'Trucks and Buses', '2,139,329.56'
+'Vintage Cars', '5,704,259.82'
