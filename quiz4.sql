8fc31db8bada37fb65838839f2f73a7ed200ad7e
Substitutional <32260164+Substitutional@users.noreply.github.com>
2018-09-26 14:17:01 +0200

    Update quiz4.sql
    rättat queryn

--- quiz4.sql
+++ quiz4.sql
@@ -1,16 +1,18 @@
 -- Query
 
-
-SELECT customers.country, SUM(orderSum.totalPriceSum) AS countryOrderSum FROM customers JOIN
-(SELECT orders.customerNumber AS customerNumber, SUM(orderdetails.priceEach*orderdetails.quantityOrdered) AS totalPriceSum
-FROM orders JOIN orderdetails
+SELECT customers.country, SUM(orderSum.totalPriceSum) AS countryOrderSum FROM customers 
+JOIN 
+(SELECT orders.customerNumber AS customerNumber, 
+SUM(orderdetails.priceEach*orderdetails.quantityOrdered) AS totalPriceSum
+FROM orderdetails JOIN orders ON orderdetails.orderNumber = orders.orderNumber
 WHERE Year(orders.orderDate) = 2005 GROUP BY orders.customerNumber
-) AS orderSum GROUP BY customers.country ORDER BY countryOrderSum DESC LIMIT 5
+) AS orderSum ON customers.customerNumber = orderSum.customerNumber 
+GROUP BY customers.country ORDER BY countryOrderSum DESC LIMIT 5
 
 -- result--
-#country, countryOrderSum
-'USA','22128055165.44'
-'Germany','7990686587.52'
-'France','7376018388.48'
-'Spain','4302677393.28'
-'UK','3073340995.20'
+# country, countryOrderSum
+'USA', '574579.81'
+'Spain', '290018.52'
+'France', '217347.62'
+'New Zealand', '165343.66'
+'Australia', '131561.38'
